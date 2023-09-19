1dbfdf79978ab8b81102676c74a1dfede6ef939a
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-09-19 19:20:54 +0600

    added navigation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
       <Navbar />
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? <p>Loading...</p> : <Outlet></Outlet>}
     </div>
   );
 };
